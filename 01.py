e252488e40353338397d4c6fa30735b385129e31
Steven Clark <steven@macbook-pro.lan>
2022-12-01 11:44:29 -0500

    tidy a bit

--- 01.py
+++ 01.py
@@ -5,14 +5,13 @@ from aocd.models import Puzzle
 def main():
     puzzle = Puzzle(year=2022, day=1)
 
-    s = puzzle.input_data
-    groups = s.split('\n\n')
+    groups = puzzle.input_data.split('\n\n')
     sums = [sum(np.fromstring(g, dtype=int, sep='\n')) for g in groups]
 
     puzzle.answer_a = max(sums)
 
     sums.sort()
-    puzzle.answer_b = np.sum(sums[-3:])
+    puzzle.answer_b = sum(sums[-3:])
 
 
 if __name__ == "__main__":
