8a6f6b1a8346c9be8e985ff35edc4ba37d3d711e
Steven Clark <steven@macbook-pro.lan>
2022-12-03 08:24:19 -0500

    adjust rucksack whitespace

--- 03.py
+++ 03.py
@@ -6,13 +6,11 @@ def grouper(iterable, n):
     args = [iter(iterable)] * n
     return zip_longest(*args)
 
-
 def get_shared_char(s):
     n = len(s)
     s1 = s[:n//2]
     s2 = s[n//2:]
     return set(s1).intersection(set(s2)).pop()
-
 
 def get_priority(c):
     if 'a' <= c <= 'z':
@@ -21,18 +19,14 @@ def get_priority(c):
         offset = ord('A')-27
     return ord(c)-offset
 
-
 def score_bag(s):
     return get_priority(get_shared_char(s))
-
 
 def get_badge(s1, s2, s3):
     return set(s1).intersection(set(s2)).intersection(set(s3)).pop()
 
-
 def get_badge_score(group):
     return get_priority(get_badge(*group))
-
 
 def main():
     puzzle = Puzzle(year=2022, day=3)
